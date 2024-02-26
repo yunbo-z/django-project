971cecf49aee42bd00786f2c401b6521afbb3c63
yunbo-z <zyunbo0330@gmail.com>
2024-02-26 19:04:31 +0100

    add detail page with dynamic path

--- meetups/urls.py
+++ meetups/urls.py
@@ -3,5 +3,6 @@ from . import views
 # this variable must be called urlpatterns
 # because django will look for this variable in urls.py file
 urlpatterns = [
-    path('meetups/', views.index)#ourdomain.com/meetups
+    path('meetups/', views.index),#ourdomain.com/meetups
+    path('meetups/<slug:meetup_slug>',views.meetup_details),
 ]
